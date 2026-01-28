4e296fc65fd4ca150a83cb30d29e73c5a15d9043
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2026-01-28 14:35:29 +0700

    get access token from cookie

--- app/_shared/libs/jwt/auth-filter.ts
+++ app/_shared/libs/jwt/auth-filter.ts
@@ -2,11 +2,12 @@ import 'server-only';
 import { AppError } from '@/app/_shared/errors/AppError';
 import { verifyToken } from './jwt';
 import { Permission } from '@/app/_shared/enums/permission.enum';
+import { cookies } from 'next/headers';
 
-export async function filter(req: Request, roles?: Permission[]) {
-    const authHeader = req.headers.get('authorization');
+export async function filter(roles?: Permission[]) {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('access_token')?.value;
 
-    const token = authHeader?.startsWith('Bearer ') ? authHeader.slice(7) : null;
     if (!token) throw new AppError('Authentication required', 401);
 
     const payload = await verifyToken(token);
