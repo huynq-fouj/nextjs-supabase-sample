e432683106b1ae894ce5d59a8277b68da23aa249
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2026-01-28 13:16:47 +0700

    update

--- app/(pages)/(sites)/layout.tsx
+++ app/(pages)/(sites)/layout.tsx
@@ -2,13 +2,12 @@ import { ReactNode } from "react";
 import Sidebar from "./_layouts/Sidebar";
 import Footer from "./_layouts/Footer";
 import Header from "./_layouts/Header";
-import Background from "./_layouts/Background";
 
 export default function SitesLayout({ children }: Readonly<{ children: ReactNode }>) {
     
     return (
         <div className="relative">
-            <Background />
+            {/* <Background /> */}
             <div className="flex relative z-10">
                 <Sidebar />
                 <div className="grow min-w-0 pt-12 flex flex-col min-h-lvh relative">
